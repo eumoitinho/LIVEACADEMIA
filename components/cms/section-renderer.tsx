58f6798f2bec2f235c2de216f548929579a3ee95
eumoitinho <joao.silva489@academico.ufgd.edu.br>
2025-10-07 04:30:34 -0300

    feat: update section renderer to use correct components and pass section data

--- components/cms/section-renderer.tsx
+++ components/cms/section-renderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroSection from '@/components/hero-section'
 import AboutSection from '@/components/about-section'
-import UnidadesCarousel from '@/components/unidades-carousel'
+import UnidadesSection from '@/components/unidades-section'
 import BeneficiosSection from '@/components/beneficios-section'
 import EstruturaSection from '@/components/estrutura-section'
 import ModalidadesSection from '@/components/modalidades-section'
@@ -10,6 +10,7 @@ import AppSection from '@/components/app-section'
 import TestimonialSection from '@/components/testimonial-section'
 import BioimpedanciaSection from '@/components/bioimpedancia-section'
 import WellhubSection from '@/components/wellhub-section'
+import ContatoSection from '@/components/contato-section'
 
 // Enquanto os componentes ainda não aceitam props dinâmicos, renderizamos os existentes.
 // Depois faremos variantes CMS-friendly.
@@ -21,17 +22,19 @@ export function renderSection(section: any, index: number) {
     case 'aboutSection':
       return <AboutSection key={index} data={section} />
     case 'unidadesSection':
-      return <UnidadesCarousel key={index} />
+      return <UnidadesSection key={index} data={section} />
     case 'beneficiosSection':
       return <BeneficiosSection key={index} data={section} />
     case 'estruturaSection':
-      return <EstruturaSection key={index} />
+      return <EstruturaSection key={index} data={section} />
     case 'modalidadesSection':
-      return <ModalidadesSection key={index} />
+      return <ModalidadesSection key={index} data={section} />
     case 'planosSection':
       return <PlanosSection key={index} data={section} />
     case 'appSection':
-      return <AppSection key={index} />
+      return <AppSection key={index} data={section} />
+    case 'contatoSection':
+      return <ContatoSection key={index} data={section} />
     case 'testimonialsSection':
       return <TestimonialSection key={index} data={section} />
     case 'bioimpedanciaSection':
