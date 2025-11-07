cd25988903e5d2d7d624a9bfade75cec3c2654a6
eumoitinho <joao.silva489@academico.ufgd.edu.br>
2025-11-07 13:35:17 -0300

    fix: improve debugging in HomepageEditable component with additional console logs

---
+++ dist/vendor/react-dom/static.browser-DoW3yqYs.mjs
@@ -0,0 +1,7 @@
+import { default as a, prerender as d, renderToReadableStream as o, version as n } from "./static-COcxs1Ma.mjs";
+export {
+  a as default,
+  d as prerender,
+  o as renderToReadableStream,
+  n as version
+};
